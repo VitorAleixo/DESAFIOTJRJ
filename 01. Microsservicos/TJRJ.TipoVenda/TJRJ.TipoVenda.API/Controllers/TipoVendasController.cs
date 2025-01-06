a32911293756f15098d683ea9e96cb4caa22fce2
Vitor Aleixo <dittgp@hotmail.com>
2025-01-06 19:49:57 -0300

    Ajustes

---
+++ 01. Microsservicos/TJRJ.TipoVenda/TJRJ.TipoVenda.API/Controllers/TipoVendasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TJRJ.TipoVenda.API.Controllers
+{
+    public class TipoVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
