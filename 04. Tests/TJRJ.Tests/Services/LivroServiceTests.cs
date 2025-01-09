3014e827e61e86a23458a088c411d8a931b6c325
VitorAleixo <dittgp@gmail.com>
2025-01-09 12:13:24 -0300

    TDD

---
+++ 04. Tests/TJRJ.Tests/Services/LivroServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TJRJ.Livros.Application.Interfaces;
+using TJRJ.Livros.Application.UseCases;
+using TJRJ.Livros.Domain.Entities;
+
+namespace TJRJ.Tests.Services
+{
+    public class LivroServiceTests
+    {
+        private Mock<ILivroRepository> _livroRepositoryMock;
+        private LivroUseCase _livroService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _livroRepositoryMock = new Mock<ILivroRepository>();
+            _livroService = new LivroUseCase(_livroRepositoryMock.Object);
+        }
+
+        [Test]
+        public void ObterTodosAsync_ReturnsAtLeastOneLivro()
+        {
+            var livros = _livroService.ObterTodosAsync();
+
+            Assert.IsNotNull(livros, "Esperado que uma acordao válido seja retornado.");
+        }
+
+    }
+}
