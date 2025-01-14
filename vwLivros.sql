93482df2ca9446e46aafb21f389e947cc5395905
Vitor Aleixo Nascimento <39105456+VitorAleixo@users.noreply.github.com>
2025-01-13 21:38:49 -0300

    Update vwLivros.sql

--- vwLivros.sql
+++ vwLivros.sql
@@ -1,2 +1,11 @@
-CREATE VIEW vwLivros  AS SELECT Nome as NomeAutor, Titulo, Editora, AnoPublicacao, Edicao, e.Descricao as Assunto FROM Autor a (NOLOCK) INNER JOIN LivroAutor b (NOLOCK) ON a.CodAu = b.Autor_CodAu INNER JOIN Livro c (NOLOCK)  ON c.CodI = b.Livro_CodI INNER
- JOIN LivroAssunto d (NOLOCK) ON d.Livro_CodI = c.CodI INNER JOIN Assunto e(NOLOCK) ON d.Assunto_codAs = e.codAs
+CREATE VIEW vwLivros  AS 
+
+SELECT Nome as NomeAutor
+     , Titulo
+     , Editora
+     , AnoPublicacao
+     , Edicao
+     , STUFF((SELECT ',' + Descricao FROM Assunto x (NOLOCK) INNER JOIN LivroAssunto y (NOLOCK) on x.codAs = y.Assunto_codAs AND y.Livro_CodI = c.CodI FOR XML PATH ('')), 1, 1, '') as Assunto 
+  FROM Autor a (NOLOCK) 
+ INNER JOIN LivroAutor b (NOLOCK) ON a.CodAu = b.Autor_CodAu 
+ INNER JOIN Livro c (NOLOCK)  ON c.CodI = b.Livro_CodI 
